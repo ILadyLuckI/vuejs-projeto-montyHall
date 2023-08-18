90ad5c379a5c0ddab67bc4f7932db779deb2ef66
Daiana Koblitz <69736274+ILadyLuckI@users.noreply.github.com>
2023-08-18 12:39:04 -0300

    Arquivos adicionados via upload

---
+++ projeto-monty-hall/src/components/Door.vue
@@ -0,0 +1,124 @@
+<template>
+    <!--flex-box-->
+    <div class="door-area">
+        <!--flex-item / moldura da porta-->
+        <div class="door-frame" :class="{ selected: selected && !open }">
+            <!--o presente so será renderizado se a porta estiver aberta e o hasGift for verdadeiro-->
+            <Gift v-if="open && hasGift" />
+        </div>
+        <!--flex-item-->
+        <div class="door" :class="{ open }"
+            @click="selected = !selected">
+            <div class="number" :class="{ selected }">{{ number }}</div>
+            <div class="knob" :class="{ selected }" 
+            @click.stop="open = true"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Gift from './Gift.vue'
+
+export default {
+    name: 'Door',
+    components: { Gift },
+    //propriedades recebidas
+    props: {
+        //o numero será tratado como texto, apenas para ser exibido na porta
+        number: {},
+        hasGift: { type: Boolean }
+    },
+    /* quando trabalhamos com componentes a propriedade 'data',
+    será uma função, para que ela possa ser invocada e para cada intância do 
+    componente ele crie um novo objeto e um estado inicial para ele*/
+    data: function() {
+        //no momento inicial que a porta for criada ela não estará nem aberta e nem selecionada
+        return {
+            //saber se a porta está fechada ou não
+            open: false,
+            //saber se tem um presente ou não
+            selected: false
+        }
+    }
+}
+</script>
+
+<style>
+:root {
+    --door-border: 5px solid brown;
+    --selected-border: 5px solid yellow;
+}
+
+.door-area {
+    position: relative;
+    width: 200px;
+    height: 310px;
+    border-bottom: 10px solid #aaa;
+    margin-bottom: 20px;
+    font-size: 3rem;
+    display: flex;
+    justify-content: center;/*centraliza os flex-items que pertencem a porta*/
+}
+
+.door-frame {
+    position: absolute;
+    height: 300px;
+    width: 180px;
+    border-left: var(--door-border);
+    border-top: var(--door-border);
+    border-right: var(--door-border);
+    display: flex;
+
+    /*presente*/
+    justify-content: center;
+    align-items: flex-end;
+}
+
+.door {
+    position: absolute;
+    top: 4.5px;
+    height: 295px;
+    width: 170px;
+    background-color: #e19056;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+}
+
+.door .knob {
+    height: 20px;
+    width: 20px;
+    border-radius: 10px;
+    background-color: brown;
+    align-self: flex-start;
+    margin-top: 60px;
+}
+
+.door-frame.selected {
+    border-left: var(--selected-border);
+    border-top: var(--selected-border);
+    border-right: var(--selected-border);
+    box-shadow: 0 0 80px rgba(255, 255, 0, 1);
+}
+
+.door .number.selected {
+    color: yellow;
+}
+
+.door .knob.selected {
+    background-color: yellow;
+}
+
+.door.open {
+    background-color: #0007;
+}
+
+.door.open .knob {
+    display: none;
+}
+
+.door.open .number {
+    display: none;
+}
+</style>
---
+++ projeto-monty-hall/src/components/Gift.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="gift">
+    <img class="img-gift" src="../../assets/Lovepik_com-401489059-vector-gift-box.png" alt="imagem de dois presentes">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Gift'
+}
+</script>
+
+<style>
+.gift {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    z-index: 10;
+}
+.img-gift {
+    width: 15rem;
+    height: auto;    
+}
+
+</style>
